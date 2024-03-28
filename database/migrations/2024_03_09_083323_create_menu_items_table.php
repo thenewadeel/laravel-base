3ef51ef2fd5d18b6aeea3d01193dd85173e14a58
Ad <thenewadeel@gmail.com>
2024-03-28 14:45:20 +0500

    menuItem added

---
+++ database/migrations/2024_03_09_083323_create_menu_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menu_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->string('title');
+            $table->string('code')->unique();
+            $table->float('price');
+            $table->float('cost');
+            $table->timestamp('added_on')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menu_items');
+    }
+};
